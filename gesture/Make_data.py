18a96f971b527137701274aa71a81070d51117b4
kim-mini <72716397+kim-mini@users.noreply.github.com>
2021-02-25 17:58:13 +0900

    0225

---
+++ gesture/Make_data.py
@@ -0,0 +1,35 @@
+import cv2
+import os
+
+# data_file path
+file_name = 'go_away'# stop,
+file_path = os.path.join( './data',file_name )
+if not os.path.exists( file_path ):
+    os.mkdir( file_path )
+
+
+cnt = 0
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    cv2.imshow('Video', frame)
+
+    imageFile = os.path.join( file_path, str(cnt) + '.jpg' )
+    if cnt%40:
+        cv2.imwrite( imageFile, frame )
+        cnt = '{}'.format( int(cnt) + 1 )
+
+
+    if len(os.listdir( file_path )) == 30:
+        break
+
+    key = cv2.waitKey( 1 ) & 0xff
+    if key == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
